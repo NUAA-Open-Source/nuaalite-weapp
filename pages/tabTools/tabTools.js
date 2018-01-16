9d42bb10bb710c1e5623d3b14518f5f91de7f288
rvfu98 <rvfu98@gmail.com>
2018-01-16 21:50:12 +0800

    Modified Tools and some pictures of pichu

--- pages/tabTools/tabTools.js
+++ pages/tabTools/tabTools.js
@@ -1,3 +1,10 @@
 // pages/tabTools/tabTools.js
 Page({
+  data: {
+    banners: [
+      'https://img.rvfu98.com/18-1-16/15813222.jpg',
+      'https://img.rvfu98.com/18-1-16/30918636.jpg',
+      'https://img.rvfu98.com/18-1-16/25436229.jpg'
+    ]
+  }
 })

aaf3436a9b4690a7d9cb125eb93fbfe9351e5d1a
rvfu98 <rvfu98@gmail.com>
2018-02-01 11:32:00 +0800

    update

--- pages/tabTools/tabTools.js
+++ pages/tabTools/tabTools.js
@@ -2,8 +2,8 @@
 Page({
   data: {
     banners: [
-      'https://img.rvfu98.com/18-1-16/15813222.jpg',
-      'https://img.rvfu98.com/18-1-16/30918636.jpg',
+      'https://img.rvfu98.com/18-1-17/90940455.jpg',
+      'https://img.rvfu98.com/18-1-17/7818367.jpg',
       'https://img.rvfu98.com/18-1-16/25436229.jpg'
     ]
   }
